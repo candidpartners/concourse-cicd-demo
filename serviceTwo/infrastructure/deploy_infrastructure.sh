ed8c22c4720d5e21bc1a36ac613be5bf2571a432
Dustin Harrell <dustin.harrell@candidpartners.com>
2020-04-24 06:12:45 -0400

    all cloudformation alternatives commited

--- serviceTwo/infrastructure/deploy_infrastructure.sh
+++ serviceTwo/infrastructure/deploy_infrastructure.sh
@@ -4,15 +4,24 @@
 SETTINGS=../../../settings
 TFOUTPUT=../../../tfoutput
 
+SERVICE="serviceTwo"
+
 ENV_NAME=`jq -r .environment $SETTINGS/settings.json`
 STATE_BUCKET=`jq -r .stateBucket $SETTINGS/settings.json`
 TF_STATE_KEY=`jq -r .tfStateKey $SETTINGS/settings.json`
 REGION=`jq -r .region $SETTINGS/settings.json`
+ROLE_ARN=`jq -r .roleArn $SETTINGS/settings.json`
 
-terraform init \
-          -backend-config "bucket=${STATE_BUCKET}" \
-          -backend-config "key=${TF_STATE_KEY}/serviceTwo/${ENV_NAME}.tfstate" \
-          -backend-config "region=${REGION}"
+aws cloudformation create-stack \
+--region $REGION \
+--stack-name $ENV_NAME-$SERVICE \
+--template-body file://cloudformation.yaml \
+--role-arn $ROLE_ARN\
+--parameters ParameterKey=environment,ParameterValue=$ENV_NAME
 
-terraform apply -auto-approve -var-file="$SETTINGS/settings.json"
-terraform output -json > $TFOUTPUT/output.json
+sleep 30
+
+DYNARN=$(aws cloudformation describe-stacks --region us-east-2 --stack-name $SERVICE | jq -r '.Stacks[0].Outputs[0].OutputValue')
+KINARN=$(aws cloudformation describe-stacks --region us-east-2 --stack-name $SERVICE | jq -r '.Stacks[0].Outputs[1].OutputValue')
+
+jq -n --arg dyn "$DYNARN" --arg kin "$KINARN" '{"dynamo_table":$dyn, "kinesis_stream":$kin}' > $TFOUTPUT/output.json
